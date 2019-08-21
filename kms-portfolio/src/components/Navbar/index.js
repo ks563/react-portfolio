dc7a1f114d16e393c3fd428c0d146df7815808ad
ks563 <karen.shea90@gmail.com>
2019-08-20 21:22:33 -0500

    comments for future work

--- kms-portfolio/src/components/Navbar/index.js
+++ kms-portfolio/src/components/Navbar/index.js
@@ -6,6 +6,8 @@ function Navbar() {
             <div>
                 <ul className="navbar-nav">
                     <h2>New Portfolio</h2>
+                    {/* add in Name on the left side, links for about, portfolio and contact on the right
+                    need to add link for resume too */}
                 </ul>
             </div>
         </nav>
